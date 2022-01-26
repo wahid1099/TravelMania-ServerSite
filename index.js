1a5c00fcb4f05d1fed29b115cb8a025cc04b7322
Abdul Wahid <wahdiahmed890@gmail.com>
2022-01-26 16:15:44 +0600

    add

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("welcome to Travel mania!");
+  res.send("welcome to Travel mania ........!");
 });
 
 app.listen(port, () => {
